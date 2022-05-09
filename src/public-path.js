c69f7c20ad4ef0b7ee5afaa8863b3eeb476cc8d9
houyuanzhen <houyuanzhen@ebupt.com>
2022-05-09 11:26:23 +0800

    feat：微前端子应用模块基础代码

---
+++ src/public-path.js
@@ -0,0 +1,11 @@
+(function () {
+  if (window.__POWERED_BY_QIANKUN__) {
+    if (!process.env.NODE_ENV.includes('prod')) {
+      // eslint-disable-next-line no-undef
+      __webpack_public_path__ = `//localhost:${process.env.VUE_APP_PORT}${process.env.VUE_APP_ROUTER_BASE_URL}`
+      return;
+    }
+    // eslint-disable-next-line no-undef
+    __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+  }
+})()
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h4>This is an about page 子应用</h4>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'AboutView',
+  components: {
+  }
+}
+</script>
